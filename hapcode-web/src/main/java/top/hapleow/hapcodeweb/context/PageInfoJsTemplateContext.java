af32462c2adbe714a209eb00cd2813eeb73f85d5
hapleow <hapleow@gmail.com>
2020-09-17 13:45:20 +0800

    page_add & page_info.js

---
+++ hapcode-web/src/main/java/top/hapleow/hapcodeweb/context/PageInfoJsTemplateContext.java
@@ -0,0 +1,29 @@
+package top.hapleow.hapcodeweb.context;
+
+import lombok.Data;
+import top.hapleow.hapcodecore.config.ApplicationConfig;
+import top.hapleow.hapcodecore.config.PackageConfig;
+import top.hapleow.hapcodecore.context.BasicTemplateContext;
+import top.hapleow.hapcodecore.model.TableModel;
+
+/**
+ * DAO模板模型
+ * 用于向模板传输数据
+ *
+ * @author wuyulin
+ * @date 2020/9/15
+ */
+@Data
+public class PageInfoJsTemplateContext extends BasicTemplateContext {
+
+
+    public PageInfoJsTemplateContext(TableModel table, String tablePrefix, ApplicationConfig applicationConfig) {
+
+        super(table, tablePrefix, applicationConfig);
+
+        PackageConfig packageConfig = applicationConfig.getPackageConfig();
+        this.setFileName(super.getBizEnName() + "_info.js");
+        this.setFilePath(packageConfig.getModulePath() + packageConfig.getJsPath() + packageConfig.getBizModulePath() + super.getBizEnName() + "/");
+    }
+
+}
