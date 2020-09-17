4d4d033732d4a9b80a2b10a5c41239611b792f9c
hapleow <hapleow@gmail.com>
2020-09-17 10:05:14 +0800

    model import

--- hapcode-web/src/main/java/top/hapleow/hapcodeweb/context/ModelJavaTemplateContext.java
+++ hapcode-web/src/main/java/top/hapleow/hapcodeweb/context/ModelJavaTemplateContext.java
@@ -5,6 +5,10 @@ import top.hapleow.hapcodecore.config.ApplicationConfig;
 import top.hapleow.hapcodecore.context.BasicTemplateContext;
 import top.hapleow.hapcodecore.model.TableModel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 基础模板模型
  * 用于向模板传输数据
@@ -22,12 +26,25 @@ public class ModelJavaTemplateContext extends BasicTemplateContext {
 
     private String subPackageName = "model/";
 
+    private List<String> imports = new ArrayList<>();
+
 
     public ModelJavaTemplateContext(TableModel table, String tablePrefix, ApplicationConfig applicationConfig) {
         super(table, tablePrefix, applicationConfig);
         this.packageName = applicationConfig.getPackageConfig().getPackageName() + "model";
         this.setFileName(super.getBizEnBigName() +".java");
         this.setFilePath(getFilePath() + subPackageName);
+
+
+        table.getFields().forEach(item -> {
+            String javaType = item.getJavaType();
+            if (Objects.equals(javaType,"LocalDate")){
+                imports.add("java.time.LocalDate");
+            }
+            if (Objects.equals(javaType,"LocalDateTime")){
+                imports.add("java.time.LocalDateTime");
+            }
+        });
     }
 
 }
